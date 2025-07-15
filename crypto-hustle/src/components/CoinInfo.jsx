ff32cb2f13bf90c860dfc4050c4bb6317ff9a85c
Ricardo Ortega Pacheco <ricardo.ortega97@outlook.com>
2025-07-15 01:11:35 -0400

    Lab 6: Route and chart update

--- crypto-hustle/src/components/CoinInfo.jsx
+++ crypto-hustle/src/components/CoinInfo.jsx
@@ -1,25 +1,52 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
 const CoinInfo = ({ image, name , symbol }) => {
     const [price, setPrice] = useState(null);
     
     useEffect(() => {
+        const controller = new AbortController();
+
         const getCoinPrice = async () => {
-            const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=${API_KEY}`);
+            try {
+                const response = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${symbol}&tsyms=USD&api_key=${API_KEY}`, {
+                signal: controller.signal
+            });
             const json = await response.json();
             setPrice(json);
+            } catch (error) {
+                if (error.name === 'AbortError') {
+                } else {
+                    console.error("Error fetching coin price:", error);
+                }
+            }
         }
+        
         getCoinPrice().catch(console.error);
+        return () => {
+            controller.abort();
+        }
     }, [symbol])
+
     return (
         <div>
             {price ? (
                 <ul>
                     <li className="main-list" key={symbol}>
-                        <img src={`https://www.cryptocompare.com${image}`} alt={`small icon for ${name}`} className="icons" />
-                        {name} <span className="tab"></span>
-                        {price && price.USD ? ` $${price.USD} USD` : null}
+                        <Link
+                        style={{ color: "White" }}
+                        to={`/coinDetails/${symbol}`}
+                        key={symbol}
+                        >
+                            <img
+                            className="icons"
+                            src={`https://www.cryptocompare.com${image}`}
+                            alt={`Small icon for ${name} crypto coin`}
+                            />
+                            {name}
+                            {price && price.USD ? ` $${price.USD} USD` : null}
+                        </Link>
                     </li>
                 </ul>
             ) : (
